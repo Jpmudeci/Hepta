9928e4619dfb7beb4885ca73c6104b5ed83d66d8
Daniel <is720317@iteso.mx>
2021-04-26 22:12:28 -0500

    x

--- controllers/purchaseController.js
+++ controllers/purchaseController.js
@@ -1,30 +1,29 @@
-let staticList = {
-    "purchases": [{
-        "name": "p1",
-        "inCharge": "Daniel"
-    },
-    {
-        "name": "p2",
-        "inCharge": "Daniel"
-    },
-    ]
-};
+const fs = require('fs');
+let JSONlist = require('../data/purchaseList');
 
 class PurchaseController {
     
     getList() {
-        return staticList
+        return JSONlist;
     }
 
     insertPurchase(p) {
-        console.log("inside: " + JSON.stringify(staticList.purchases[0]));
+        console.log("inside: " + JSON.stringify(JSONlist.purchases[0]));        
+        JSONlist.purchases.push(p);
+        fs.writeFile('./data/purchaseList.json', JSON.stringify(JSONlist), (err) =>{
+            if(err) throw err;
+        })
         return JSON.stringify(p);
     }
 
     deletePurchase(p) {
         console.log("Hola");
-        let purchase = JSON.stringify(staticList.purchases[p]);
-        staticList.purchases.splice[p];
+        let purchase = JSON.stringify(JSONlist.purchases[p]);
+        JSONlist.purchases.splice(p);
+        //console.log("Lista: " + JSON.stringify(JSONlist));
+        fs.writeFile('./data/purchaseList.json', JSON.stringify(JSONlist), (err) => {
+            if(err) throw err;
+        });
         return purchase;
     }
 
